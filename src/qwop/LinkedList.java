dba1355351405c897d4d008be5679a4064fe318c
Billy J.B <chrysophylaxdives@users.noreply.github.com>
2016-01-18 21:45:28 +0100

    added counter and start of javadoc
    Now we can keep track of the size of our list and write an OK doc…

--- src/qwop/LinkedList.java
+++ src/qwop/LinkedList.java
@@ -1,9 +1,16 @@
 package qwop;
 
+/*
+**
+* A simple attempt at a linked list structure.
+*
+*
+ */
 public class LinkedList<T> {
 
 	private Node<T> first;
 	private Node<T> last;
+	private int size = 0;
 
 	public void add(Node<T> node) {
 		if (first == null) {
@@ -13,17 +20,20 @@ public class LinkedList<T> {
 			last.next = node;
 			last = node;
 		}
+		size++;
 	}
 
 	public void removeAfter(Node<T> node) {
 		if (node.next != null) {
 			node.next = node.next.next;
 		}
+		size--;
 	}
 
 	public void insertAfter(Node<T> node, Node<T> newNode) {
 		newNode.next = node.next; // point new node header to old node's
 		node.next = newNode; // point old node to our newly inserted node
+		size++;
 	}
 
 }
