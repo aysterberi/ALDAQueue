64def2dcc764bd15fdce7080ea8f113ba0df3174
chrysophylaxdives <chrysophylaxdives@users.noreply.github.com>
2016-01-18 11:28:07 +0100

    Create LinkedQueue.java
    does not compile, initial blergh

---
+++ src/qwop/LinkedQueue.java
@@ -0,0 +1,53 @@
+package qwop;
+
+import qwop.LinkedList;
+import alda.linear.ALDAQueue;
+
+public class LinkedQueue<E> implements ALDAQueue<E> {
+
+	public void add(E element) {
+	}
+
+public void addAll(Collection<?> extends E> c)
+	{
+	}
+
+	public E remove() {
+		// TODO
+		return null;
+	}
+
+	public E peek() {
+		// TODO
+		return null;
+	}
+
+	public void clear() {
+	}
+
+	public int size() {
+	}
+
+	public boolean isEmpty() {
+		// TODO
+		return false;
+	}
+
+	public boolean isFull() {
+		// TODO
+		return false;
+	}
+
+public int totalCapacity( { return 0;)
+
+	public int currentCapacity() {
+		return 0;
+	}
+
+	public int discriminate(E element) {
+		// TODO
+		// move all E equal to e to the end of the queue, throw NPE if e ==
+		// null, return n(E_moved)
+	}
+
+}
