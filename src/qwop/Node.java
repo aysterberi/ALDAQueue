15b1ce260d45f594b816b5bd0665eb454a5516dc
Billy J.B <chrysophylaxdives@users.noreply.github.com>
2016-01-18 22:06:37 +0100

    constructor added

--- src/qwop/Node.java
+++ src/qwop/Node.java
@@ -1,10 +1,21 @@
 package qwop;
 
 /**
+ * A simple node capable of pointing to data and another node
  * Created by Joakim on 2016-01-18.
+ *
  */
 public class Node<T> {
 
 	public Node next;
 	public T data;
+
+	/**
+	 * Initializes a node with no reference.
+	 *
+	 * @param data the data reference the node will hold
+	 */
+	public Node(T data) {
+		this.data = data;
+	}
 }
